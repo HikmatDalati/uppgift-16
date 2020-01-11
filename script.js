431b5d2e0df882b59b14a81d8c88feae75cdb090
HikmatDalati <58488387+HikmatDalati@users.noreply.github.com>
2020-01-11 22:00:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+if (navigator.language == "sv-SE") {
+	document.getElementById("demo").innerHTML="språket som är på är svenska"
+}
+else{
+	document.getElementById("demo").innerHTML="språket som är på är inte svenska"
+};
+
+
+document.getElementById("cookie").innerHTML=
+"cookiesEnabled is " + navigator.cookiesEnabled;
+
+ document.write("Javascript är på")
